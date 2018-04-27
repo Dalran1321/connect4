a4bc41871e7c3f0437f284a98afa41334ec42829
323905018 <323905018@C-6YBT0R1.tdsb.on.ca>
2018-04-27 14:21:23 -0400

    gui update

--- ChiassonLiangConnect4/src/chiasson/mark/GUIDriver.java
+++ ChiassonLiangConnect4/src/chiasson/mark/GUIDriver.java
@@ -1,4 +1,5 @@
 package chiasson.mark;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,24 +14,35 @@ public class GUIDriver extends Application {
 
 	ColumnButton[] columnBtns = new ColumnButton[7];
 	Button[][] cellBtns = new Button[7][6]; 
+	final int ROWS = 6;
+	final int COLS = 7;
+	// create the board
+	Board board = new Board(ROWS, COLS);
+	CellState cState = CellState.P2;
 	
 	public static void main(String[] args) {
 		launch(args);
 
 	}
-
+	
 	@Override
 	public void start(Stage window) throws Exception {
 		VBox area = new VBox();
 		HBox columnSelector = new HBox();
 		for (int i=0; i<columnBtns.length; i++){
-			columnBtns[i]= new ColumnButton("=",i);
+			columnBtns[i]= new ColumnButton(String.valueOf(i+1),i+1);
 			columnSelector.getChildren().add(i,columnBtns[i]);
 		    columnBtns[i].setOnAction(new EventHandler<ActionEvent>(){
 				@Override
 				public void handle(ActionEvent event){
-				System.out.println("Column: ");
-				System.out.println(event.getEventType());
+				int c = ((ColumnButton)event.getSource()).getColumnValue();
+				if (!board.isColumnFilled(c)){
+					board.place(c, cState);
+				}
+				board.display();
+					
+				
+				
 				}
 			});
 		}
@@ -39,6 +51,7 @@ public class GUIDriver extends Application {
 		for (int row=0; row<cellBtns.length; row++){
 			for (int col =0; col<cellBtns[0].length; col++){
 				cellBtns[row][col] =new Button("+"); 
+				cellBtns[row][col].setStyle("-fx-base: #C0C0C0;");
 				gp.add(cellBtns[row][col], row,col);
 			}
 		}
